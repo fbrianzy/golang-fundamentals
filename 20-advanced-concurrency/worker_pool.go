bb48848b0a15fb1aa0a65ad492d86239255cb6c5
Febrianss. <164667693+fbrianzy@users.noreply.github.com>
2025-10-03 08:10:20 +0700

    Add files via upload

---
+++ 20-advanced-concurrency/worker_pool.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup){
+    defer wg.Done()
+    for j := range jobs { results <- j*j }
+}
+
+func main(){
+    jobs := make(chan int, 5)
+    results := make(chan int, 5)
+
+    var wg sync.WaitGroup
+    for i:=0;i<3;i++{ wg.Add(1); go worker(i, jobs, results, &wg) }
+
+    for j:=1;j<=5;j++{ jobs<-j }
+    close(jobs)
+
+    go func(){ wg.Wait(); close(results) }()
+
+    for r := range results { fmt.Println("result:", r) }
+}
